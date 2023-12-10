619d0eb4107f12fe1b5a41eee3994ea5ee3a466b
phongth <45889679+ShinlaTensei@users.noreply.github.com>
2023-12-10 21:52:13 +0700

    Update cheat menu

--- Assets/Scripts/CheatSample/CheatCommandTest.cs
+++ Assets/Scripts/CheatSample/CheatCommandTest.cs
@@ -9,14 +9,25 @@ using UnityEngine;
 public class CheatCommandTest : BaseMono
 {
     [CheatCommand("Cheat Command One", category: "Sample")]
-    private void CheatCommand_One()
+    private void CheatCommand_One(int one, bool two, string three)
     {
         PDebug.Info("Cheat Command 1");
+    }
+    
+    [CheatCommand("CheatCommand Two", category: "Sample")]
+    private void CheatCommand_Two(TestEnum testEnum, int one, float two)
+    {
+        PDebug.Info("Cheat Command 2");
     }
 
     private void Awake()
     {
-        PDebug.Info("Cheat Command 1");
+        this.RegisterCallerInstance();
+    }
+
+    public enum TestEnum
+    {
+        One, Two, Three
     }
 }
 
