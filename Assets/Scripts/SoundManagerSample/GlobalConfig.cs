133c24203230fb405e8580dee8c8ae491bf3c64f
phongth <45889679+ShinlaTensei@users.noreply.github.com>
2024-01-01 15:54:30 +0700

    Update

---
+++ Assets/Scripts/SoundManagerSample/GlobalConfig.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Base;
+using Base.Helper;
+using Base.Pattern;
+using Base.Services;
+using TMPro;
+using UnityEngine;
+
+public class GlobalConfig : MonoBehaviour
+{
+    [SerializeField] private AudioMappingConfiguration m_mappingConfiguration;
+    [SerializeField] private TMP_Text                  m_countDownText;
+
+    private TimerData m_timer;
+
+    private void Awake()
+    {
+        ServiceLocator.Get<SoundService>().Init();
+        ServiceLocator.Get<SoundService>().UpdateData(m_mappingConfiguration);
+        ServiceLocator.Get<TimingService>().SetTimeInitialize(DateTime.Now);
+        m_timer = new TimerDataMutable(ServiceLocator.Get<TimingService>().GetCurrentTime(), 3600);
+        m_timer.SetupTimer(OnTimeRunning, OnTimeFinished);
+    }
+
+    private void OnDestroy()
+    {
+        m_timer.StopRunningTimer();
+    }
+
+    private void OnTimeRunning(int secondsLeft)
+    {
+        TimeSpan timeLeft = ServiceLocator.Get<TimingService>().TimeLeftUntil(m_timer.GetFinishDateTime());
+        m_countDownText.text = string.Format("{0:00}:{1:00}", timeLeft.Minutes, timeLeft.Seconds);
+    }
+
+    private void OnTimeFinished()
+    {
+        
+    }
+}
