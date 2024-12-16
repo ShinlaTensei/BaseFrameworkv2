9fd1c655e1820409973a5daf597f796e1e2dcfd8
PhongTH <phongasd87@gmail.com>
2024-12-16 21:35:02 +0700

    Update

--- Assets/Scripts/Downloader/ImageDownloaderSample.cs
+++ Assets/Scripts/Downloader/ImageDownloaderSample.cs
@@ -1,3 +1,4 @@
+using System;
 using Base.Core;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
@@ -8,12 +9,30 @@ public class ImageDownloaderSample : MonoBehaviour
     [SerializeField] private SpriteRenderer m_spriteRenderer;
     [SerializeField] private SpriteRenderer m_spriteRenderer1;
     [SerializeField] private SpriteRenderer m_spriteRenderer2;
+
+    private Texture2D m_texture2D;
     // Start is called before the first frame update
     void Start()
     {
-        ImageDownloader.Create().Load(m_url).Into(m_spriteRenderer).AttachToken(this.GetCancellationTokenOnDestroy()).Start();
-        ImageDownloader.Create().Load(m_url).Into(m_spriteRenderer1).AttachToken(this.GetCancellationTokenOnDestroy()).Start();
-        ImageDownloader.Create().Load(m_url).Into(m_spriteRenderer2).AttachToken(this.GetCancellationTokenOnDestroy()).Start();
+        ImageDownloader.Create().Load(m_url).AttachToken(this.GetCancellationTokenOnDestroy())
+            .WithCompleted(OnLoadTextureCompleted).Start();
+    }
+
+    private void OnLoadTextureCompleted(Texture2D texture2D)
+    {
+        if (texture2D != null) m_texture2D = texture2D;
+        
+        Sprite sprite = Sprite.Create(m_texture2D, new Rect(0, 0, m_texture2D.width, m_texture2D.height), new Vector2(0.5f, 0.5f));
+        if (sprite == null) return;
+
+        m_spriteRenderer.sprite = sprite;
+        m_spriteRenderer1.sprite = sprite;
+        m_spriteRenderer2.sprite = sprite;
+    }
+
+    private void OnDestroy()
+    {
+        
     }
 
     // Update is called once per frame
